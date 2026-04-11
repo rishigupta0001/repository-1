077afd186256c44f8da11f534d55aa1466da4371
rishigupta0001 <rishizx1234@gmail.com>
2026-04-11 17:44:03 +0530

    Update a19.c

--- a19.c
+++ a19.c
@@ -7,17 +7,31 @@ struct EMPLOYEE {
 };
 
 int main() {
-    int n, m=0, f=0;
+    int n, m = 0, f = 0;
     struct EMPLOYEE e[100];
+
     printf("Enter count: ");
-    scanf("%d", &n);
+    if (scanf("%d", &n) != 1 || n <= 0 || n > 100) {
+        printf("Invalid employee count.\n");
+        return 1;
+    }
 
     for (int i = 0; i < n; i++) {
-        scanf("%s %s %s %s %f", e[i].name, e[i].desig, e[i].gen, e[i].doj, &e[i].sal);
-        if (strcmp(e[i].gen, "Male") == 0) m++;
-        else f++;
-        if (e[i].sal > 10000) printf("High salary: %s\n", e[i].name);
+        printf("Enter employee %d details (name designation gender doj salary): ", i + 1);
+        if (scanf("%49s %49s %9s %14s %f", e[i].name, e[i].desig, e[i].gen, e[i].doj, &e[i].sal) != 5) {
+            printf("Invalid input for employee %d.\n", i + 1);
+            return 1;
+        }
+
+        if (strcmp(e[i].gen, "Male") == 0)
+            m++;
+        else
+            f++;
+
+        if (e[i].sal > 10000)
+            printf("High salary: %s\n", e[i].name);
     }
+
     printf("Total: %d, Male: %d, Female: %d\n", n, m, f);
     return 0;
 }
